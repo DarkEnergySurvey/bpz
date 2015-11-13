079363d483ad56ccdbf3714b65fe7c44fb0c4df7
Ben Hoyle <hoyleb@usm.lmu.de>
2015-11-13 20:24:15 +0100

    validation script updates

--- validation/photo_z_metrics.py
+++ validation/photo_z_metrics.py
@@ -1,28 +1,37 @@
 import numpy as np
-
+import yaml
 
 class metricBins:
 
     def __init__(self, group=None):
         self._group = None
         if group == 'photoz':
-            self._group = group
+            self.group = group
             """Photo z bins go here
             """
-            self._tomographic_bins = [0.0, 3.0]
+            self.tomographic_bins = [0.0, 3.0]
 
         if group == 'weak_lensing':
             self._group = group
             """Weak lensing metrics go here
 
             """
-            self._tomographic_bins = [0.0, 0.3, 0.6, 0.9, 1.3, 2.0]
+            self.tomographic_bins = [0.0, 0.3, 0.6, 0.9, 1.3, 2.0]
 
         #otherwise _group is not set
 
-    def tomographic_bins(self):
-        return self._tomographic_bins
+    def get_param(self, ky):
+        if ky in self:
+            return self[ky]
+        return None
 
-    def group(self):
-        return self.group
-    
+    def set_param(self, param):
+        for i in param:
+            self[i] = param[i]
+
+def photoz():
+    return metricBins('photoz')
+
+def weak_lensing():
+    return metricBins('weak_lensing')
+

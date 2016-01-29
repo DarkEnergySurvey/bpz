b6f25969a603300529c339cbca9794a15c116910
christopher_bonnett <c.bonnett@gmail.com>
2016-01-29 11:11:58 +0100

    small update

--- validation/bh_photo_z_validation.py
+++ validation/bh_photo_z_validation.py
@@ -841,7 +841,7 @@ def weighted_nz_distributions(df, binning, weights=None, tomo_bins=np.array([0, 
             spec_sum_array = np.zeros_like(binning)
             for i in xrange(n_resample):
                 df_sample = df_sel.sample(n=len(df_sel), replace=True, weights=df_sel[weights])
-                kde_w_spec_pdf = gaussian_kde(df_sample['Z_SPEC'].values, bw_method='silverman')
+                kde_w_spec_pdf = gss_kde(df_sample['Z_SPEC'].values, bw_method='silverman')
                 kde_w_spec_pdf = kde_w_spec_pdf(binning)
 
                 phot_iter[j + 1][i + 1] = _normalize_pdf(df_sample[pdf_names].sum(), binning[1] - binning[0]).values
@@ -849,8 +849,8 @@ def weighted_nz_distributions(df, binning, weights=None, tomo_bins=np.array([0, 
                 phot_sum_array = phot_sum_array + phot_iter[j + 1][i + 1]
                 spec_sum_array = spec_sum_array + spec_iter[j + 1][i + 1]
             
-            phot_iter[j + 1][0] = phot_sum_array/ n_resample
-            spec_iter[j + 1][0] = spec_sum_array/ n_resample
+            phot_iter[j + 1][0] = phot_sum_array/ float(n_resample)
+            spec_iter[j + 1][0] = spec_sum_array/ float(n_resample)
             #kde_w_spec_pdf = gss_kde(df_sel['Z_SPEC'].values, bw_method='silverman', weights=df_sel[weights].values)
             #spec_iter[j + 1][0] = kde_w_spec_pdf(binning)
 
@@ -865,18 +865,20 @@ def weighted_nz_distributions(df, binning, weights=None, tomo_bins=np.array([0, 
     spec_sum_array = np.zeros_like(binning)
     for i in xrange(n_resample):
         df_sample = df_sel.sample(n=len(df_sel), replace=True, weights=df_sel[weights])
-        kde_w_spec_pdf = gaussian_kde(df_sample['Z_SPEC'].values, bw_method='silverman')
+        kde_w_spec_pdf = gss_kde(df_sample['Z_SPEC'].values, bw_method='silverman')
         kde_w_spec_pdf = kde_w_spec_pdf(binning)
 
         phot_iter[0][i + 1] = _normalize_pdf(df_sample[pdf_names].sum(), binning[1] - binning[0]).values
         spec_iter[0][i + 1] = kde_w_spec_pdf
+        phot_sum_array = phot_sum_array + phot_iter[0][i + 1]
+        spec_sum_array = spec_sum_array + spec_iter[0][i + 1]
 
-    phot_iter[0][0] = phot_sum_array/ n_resample
-    spec_iter[0][0] = spec_sum_array/ n_resample
+    phot_iter[0][0] = phot_sum_array/ float(n_resample)
+    spec_iter[0][0] = spec_sum_array/ float(n_resample)
     #phot_iter[0][0] = _normalize_pdf(df_sel[pdf_names].sum(), binning[1] - binning[0]).values
     #kde_w_spec_pdf = gss_kde(df_sel['Z_SPEC'].values, bw_method='silverman', weights=df_sel[weights].values)
     #spec_iter[0][0] = kde_w_spec_pdf(binning)
 
-    data_for_wl = {'binning': binning, 'phot': phot_iter, 'spec': spec_iter}
+    data_for_wl = {'binning': binning, 'phot': phot_iter, 'spec': spec_iter, 'tomo_bins' : tomo_bins}
 
     return phot_iter, spec_iter, data_for_wl

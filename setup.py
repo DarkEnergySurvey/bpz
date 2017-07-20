29f362f2772a4c09bfff8c8b735f7b19a058910e
Felipe Menanteau <felipe@illinois.edu>
2017-07-20 08:42:31 -0500

    tagging new version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ data_files.append(('ups',['ups/bpz.table']))
 
 # The main call
 setup(name='bpz',
-      version ='1.4.0',
+      version ='1.4.1',
       license = "GPL",
       description = "DESDM implementation of BPZ",
       author = "Ben Hoyle, Felipe Menanteau",
